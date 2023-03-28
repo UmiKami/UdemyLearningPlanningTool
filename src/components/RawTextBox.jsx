471ede5e3d61d01e54a3a824753f5e002c9a542d
UmiKami <ggoku02@gmail.com>
2023-03-28 01:10:32 -0400

    removed dependency on chatGPT

--- src/components/RawTextBox.jsx
+++ src/components/RawTextBox.jsx
@@ -1,67 +1,57 @@
 import axios from "axios";
 import { useState } from "react";
 
-
 const RawTextBox = ({ setTodoList }) => {
-    const [rawText, setRawText] = useState(localStorage.getItem("rawText") || "");
-
-    let prompt = `Create a JSON object which contains the following keys:
-
-lessons, totalTime
-
-lessons is an array of objects. Each object inside the array contains a title (with its original numbering), a duration in minutes, and a done key with a value of false.
-
-totalTime, is the sum of the durations of all the given lessons.
-
-
-Use this text to create the JSON object based on the previous specifications:\n`;
+    const [rawText, setRawText] = useState(
+        localStorage.getItem("rawText") || ""
+    );
 
     const handlePromptSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        axios
-            .post(
-                "https://api.openai.com/v1/chat/completions",
-                {
-                    model: "gpt-3.5-turbo",
-                    messages: [{role: 'user', content: prompt + rawText}],
-                    max_tokens: 300,
-                    temperature: 0,
-                    top_p: 1,
-                    n: 1,
-                    stream: false,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${
-                            import.meta.env.VITE_OPENAI_API_KEY
-                        }`,
-                    },
+        const lessons = [];
+        let totalTime = 0;
+
+        const lines = rawText.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            const titleLine = lines[i];
+            const titleRegex = /^\d{1,3}\..+/;
+
+            if (titleRegex.test(titleLine)) {
+                const durationLine = lines[++i];
+                const durationRegex = /^\d{1,3}min$/;
+
+                if (durationRegex.test(durationLine)) {
+                    const duration = parseInt(durationLine);
+                    lessons.push({ title: titleLine, duration, done: false });
+                    totalTime += duration;
+                } else {
+                    throw new Error(
+                        `Invalid duration format at line ${
+                            i + 1
+                        }: ${durationLine}`
+                    );
                 }
-            )
-            .then((res) => {
-                
-                let lessonList = JSON.parse(res.data.choices[0].message.content)
-                
-                console.log(lessonList);
+            } else if (titleLine.trim().toLowerCase() !== "play") {
+                throw new Error(
+                    `Invalid title format at line ${i + 1}: ${titleLine}`
+                );
+            }
+        }
 
-                setTodoList(lessonList);
-                localStorage.setItem("rawText", rawText);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-    }
-
+        setTodoList({
+            lessons,
+            totalTime,
+        });
+        localStorage.setItem("rawText", rawText);
+    };
 
     return (
         <div className="mb-5">
-    
             <form className="mb-3" onSubmit={handlePromptSubmit}>
                 <label for="exampleFormControlTextarea1" className="form-label">
-                    Copy and paste the information about of the lessons you want to
-                do here:
+                    Copy and paste the information about of the lessons you want
+                    to do here:
                 </label>
                 <textarea
                     className="form-control"
@@ -69,9 +59,11 @@ Use this text to create the JSON object based on the previous specifications:\n`
                     rows="6"
                     value={rawText}
                     onChange={(e) => setRawText(e.target.value)}
-                    style={{resize: "none"}}
+                    style={{ resize: "none" }}
                 ></textarea>
-                <button type="submit" className="btn btn-success mt-3">Create List</button>
+                <button type="submit" className="btn btn-success mt-3">
+                    Create List
+                </button>
             </form>
         </div>
     );
