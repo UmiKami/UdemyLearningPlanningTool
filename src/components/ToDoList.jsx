6b6a7dfa28c182d327a7b3bb7038eca0a086886e
UmiKami <ggoku02@gmail.com>
2023-03-28 10:23:56 -0400

    User is now able to convert minutes to hours and minutes by clicking on the time

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,9 +3,15 @@ import "../styles/ToDoList.css"
 
 const ToDoList = ({ todoList, setTodoList, saveList, removeList }) => {
     let tasks = todoList.lessons || []
-    console.log("Todos from TodoList",todoList.totalTime);
 
     const [time, setTime] = useState(0)
+    const [timeMode, setTimeMode] = useState("simple")
+
+    const formatMinutesToHours = (minutes) => {
+        const hours = Math.floor(minutes / 60);
+        const remainingMinutes = minutes % 60;
+        return `${hours} h ${remainingMinutes}`;
+    };
 
     const changeCurrentTaskStatus = (status, idx) => {
         setTodoList(state => {
@@ -38,23 +44,48 @@ const ToDoList = ({ todoList, setTodoList, saveList, removeList }) => {
 
     return (
         <div className="py-3">
-            <div className="mb-4 d-flex justify-content-between align-items-center" >
+            <div className="mb-4 d-flex justify-content-between align-items-center">
                 <h4>
                     Total Time:{" "}
-                    <span
-                        className={
-                            todoList.length <= 0 ? "text-danger" : "text-success number-change"
-                        }
-                    >
-                        {todoList.totalTime ? time : 0} mins
-                    </span>
+                    {timeMode == "simple" ? (
+                        <span
+                            role="button"
+                            className={
+                                time <= 0
+                                    ? "text-danger"
+                                    : "text-success number-change text-decoration-underline"
+                            }
+                            onClick={() => setTimeMode("complex")}
+                        >
+                            {todoList.totalTime ? time : 0} mins
+                        </span>
+                    ) : (
+                        timeMode == "complex" && (
+                            <span
+                                role="button"
+                                className={
+                                    time <= 0
+                                        ? "text-danger"
+                                        : "text-success number-change text-decoration-underline"
+                                }
+                                onClick={() => setTimeMode("simple")}
+                            >
+                                {todoList.totalTime
+                                    ? formatMinutesToHours(time)
+                                    : 0}{" "}
+                                mins
+                            </span>
+                        )
+                    )}
                 </h4>
-                <button className="btn btn-danger" onClick={removeList}> Clear List </button>
+                <button className="btn btn-danger" onClick={removeList}>
+                    {" "}
+                    Clear List{" "}
+                </button>
             </div>
 
             {tasks.length ? (
                 tasks.map((task, idx) => {
-
                     return (
                         <div
                             className={
